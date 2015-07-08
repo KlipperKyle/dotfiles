3026c69d2787fba5b38d1e61f679f62f29e349cd
Kyle Terrien <kyleterrien@gmail.com>
2015-07-07 20:51:38 -0700

    backup-wopr.sh

---
+++ bin/backup-wopr.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+
+if [ "$UID" -ne 0 ] ; then
+    echo "$0: Must run as root" >&2
+    exit 1
+fi
+
+rsync -aAXHv \
+    --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/srv/*","/lost+found","/backup/*","/home/*/.cache/*","/home/*/.ccache/*","/home/*/.thumbnails/*","/home/*/.gvfs"} \
+    --delete \
+    / /backup/wopr/
