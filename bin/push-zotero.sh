4cbed346674f9ecca130e21bd93932000e4ef879
Kyle Terrien <kyleterrien@gmail.com>
2016-01-24 20:10:56 -0800

    push/pull zotero data

---
+++ bin/push-zotero.sh
@@ -0,0 +1,29 @@
+#!/bin/bash
+
+# Push Standalone Zotero data to another host
+# Usage: push-zotero.sh [-P <profile>] [user@]host
+
+function fatal {
+    echo "ERROR: $@" 1>&2
+    echo "Usage: $0 [-P <profile>] [user@]host"
+    exit 1
+}
+
+profile='default'
+host="$1"
+
+if [[ "$1" = "-P" ]] ; then
+    profile=$2
+    host=$3
+fi
+
+[[ ! -z "$profile" ]] || fatal "Missing Profile"
+[[ ! -z "$host" ]] || fatal "Missing Hostname"
+
+profile_dir=".zotero/zotero/$profile"
+
+[[ -h "$HOME/$profile_dir/lock" ]] && fatal "Profile $profile is running.  Exit Zotero."
+ssh "$host" test -h "$profile_dir/lock" && fatal "Profile $profile is running on remote host.  Exit remote Zotero."
+
+set -x
+rsync -av --delete "$HOME/$profile_dir/" "$host:$profile_dir/"
