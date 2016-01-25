2bfd9af42cbe845df2177415e4932cc52a245e03
Kyle Terrien <kyleterrien@gmail.com>
2016-01-24 18:35:29 -0800

    {push,pull}-seamonkey.sh scripts: Copy SeaMonkey profile over a network

---
+++ bin/push-seamonkey.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+# Push a SeaMonkey profile to another host
+# Usage: push-seamonkey.sh [-P <profile>] [user@]host
+
+function fatal {
+    echo "$@" 1>&2
+    echo "Usage: $0 [-P <profile>] [user@]host"
+    exit 1
+}
+
+profile='default'
+host="$1"
+
+if [[ "$1" = "-P" ]] ; then
+    profile=$2
+    host=$3
+fi
+
+[[ ! -z "$profile" ]] || fatal "Missing Profile"
+[[ ! -z "$host" ]] || fatal "Missing Hostname"
+
+profile_dir=".mozilla/seamonkey/$profile"
+
+[[ -h "$HOME/$profile_dir/lock" ]] && fatal "Profile $profile is running.  Exit SeaMonkey."
+
+set -x
+rsync -av --delete "$HOME/$profile_dir/" "$host:$profile_dir/"
