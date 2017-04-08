2cddd4531a57bde3088c24819d85ae72642d5a5b
Kyle Terrien <kyleterrien@gmail.com>
2017-04-07 17:57:09 -0700

    Updates to backup-wopr.sh

--- bin/backup-wopr.sh
+++ bin/backup-wopr.sh
@@ -5,7 +5,7 @@ if [ "$UID" -ne 0 ] ; then
     exit 1
 fi
 
-rsync -aAXHv \
-    --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/srv/*","/lost+found","/backup/*","/home/*/.cache/*","/home/*/.ccache/*","/home/*/.thumbnails/*","/home/*/.gvfs"} \
+rsync -aAXHSv \
+    --exclude={"/dev/*","/proc/*","/sys/*","/tmp/*","/run/*","/mnt/*","/media/*","/lost+found","/home/*/.cache/*","/home/*/.ccache/*","/home/*/.thumbnails/*","/home/*/.gvfs"} \
     --delete \
-    / /backup/wopr/
+    / /mnt/backup/wopr/
