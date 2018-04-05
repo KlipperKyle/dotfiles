a975e0b17154eb0c32c6d82d38a910df253cc0db
Kyle Terrien <kyleterrien@gmail.com>
2018-04-05 14:19:13 -0700

    Add basilisk-link.sh script

--- bin/basilisk-link.sh
+++ bin/basilisk-link.sh
@@ -1,9 +1,10 @@
 #!/bin/sh
 #
-# palemoon-link.sh
+# basilisk-link.sh
 #
 # Run this as root.
-# Assumption: you have already extracted the Pale Moon build to /opt
+# Assumption: you have already extracted the Basilisk build to /opt
+# <https://www.basilisk-browser.org/download.shtml>
 # <http://linux.palemoon.org/download/mainline/>
 # <http://linux.palemoon.org/help/installation/>
 
@@ -13,31 +14,31 @@ if [ "$USER" != "root" ] ; then
 fi
 
 # Binary
-ln -s /opt/palemoon/palemoon /usr/local/bin/palemoon
+ln -s /opt/basilisk/basilisk /usr/local/bin/basilisk
 
 # Icons
 mkdir -p /usr/local/share/icons/hicolor/16x16/apps/
 mkdir -p /usr/local/share/icons/hicolor/32x32/apps/
 mkdir -p /usr/local/share/icons/hicolor/48x48/apps/
 mkdir -p /usr/local/share/icons/hicolor/128x128/apps/
-ln -s /opt/palemoon/browser/chrome/icons/default/default16.png /usr/local/share/icons/hicolor/16x16/apps/palemoon.png
-ln -s /opt/palemoon/browser/chrome/icons/default/default32.png /usr/local/share/icons/hicolor/32x32/apps/palemoon.png
-ln -s /opt/palemoon/browser/chrome/icons/default/default48.png /usr/local/share/icons/hicolor/48x48/apps/palemoon.png
-ln -s /opt/palemoon/browser/icons/mozicon128.png /usr/local/share/icons/hicolor/128x128/apps/palemoon.png
+ln -s /opt/basilisk/browser/chrome/icons/default/default16.png /usr/local/share/icons/hicolor/16x16/apps/basilisk.png
+ln -s /opt/basilisk/browser/chrome/icons/default/default32.png /usr/local/share/icons/hicolor/32x32/apps/basilisk.png
+ln -s /opt/basilisk/browser/chrome/icons/default/default48.png /usr/local/share/icons/hicolor/48x48/apps/basilisk.png
+ln -s /opt/basilisk/browser/icons/mozicon128.png /usr/local/share/icons/hicolor/128x128/apps/basilisk.png
 
 # Desktop file
 [ -d /usr/local/share/applications/ ] || mkdir -p /usr/local/share/applications/
-cat > /usr/local/share/applications/palemoon.desktop << "EOF"
+cat > /usr/local/share/applications/basilisk.desktop << "EOF"
 [Desktop Entry]
 Version=1.0
-Name=Pale Moon Web Browser
+Name=Basilisk Web Browser
 Comment=Browse the World Wide Web
 Keywords=Internet;WWW;Browser;Web;Explorer
-Exec=palemoon %u
+Exec=basilisk %u
 Terminal=false
 X-MultipleArgs=false
 Type=Application
-Icon=palemoon
+Icon=basilisk
 Categories=Network;WebBrowser;
 MimeType=text/html;text/xml;application/xhtml+xml;application/xml;application/rss+xml;application/rdf+xml;image/gif;image/jpeg;image/png;x-scheme-handler/http;x-scheme-handler/https;x-scheme-handler/ftp;x-scheme-handler/chrome;video/webm;application/x-xpinstall;
 StartupNotify=true
