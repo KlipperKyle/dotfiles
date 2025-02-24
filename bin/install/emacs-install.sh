ca35c67cc58ff2017f1e8d48659ff2004d01eb65
Kyle Terrien <kyle@terren.us>
2025-02-23 23:18:54 -0800

    Add emacs install script

---
+++ bin/install/emacs-install.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+set -e
+set -x
+
+d="/usr/local/stow/emacs-30.1-lucid1"
+
+make install "prefix=$d"
+cd "$d/bin"
+mv -v ctags ctags-emacs
+mv -v etags etags-emacs
+cd ../share/man/man1
+mv -v ctags.1.gz ctags-emacs.1.gz
+mv -v etags.1.gz etags-emacs.1.gz
+echo '.so man1/etags-emacs.1' | gzip -c | tee ctags-emacs.1.gz > /dev/null
+cd
+sudo chown -R root:root "$d"
