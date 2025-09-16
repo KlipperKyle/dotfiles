6fec469a3dc7bf6572de503b4c70adefe95d399f
Kyle Terrien <kyle@terren.us>
2025-09-15 22:11:39 -0700

    Emacs: enable narrowing commands

--- emacs.d/init.el
+++ emacs.d/init.el
@@ -318,3 +318,5 @@ See https://www.emacswiki.org/emacs/NoTabs"
 (put 'downcase-region 'disabled nil)
 (put 'upcase-region 'disabled nil)
 (put 'set-goal-column 'disabled nil)
+(put 'narrow-to-region 'disabled nil)
+(put 'narrow-to-page 'disabled nil)
